5785c78d76b3cdcf76cc66515ced9627e3e1c6e4
davidbrijak <61884037+davidbrijak@users.noreply.github.com>
2022-11-28 09:24:46 +0100

    Delete FE directory

--- FE/klinikaStudyBase/src/app/dto/UserTokensDto.ts
+++
@@ -1,8 +0,0 @@
-class TokenDto {
-  token: string;
-}
-
-export class UserTokensDto {
-  accessToken: TokenDto;
-  refreshToken: TokenDto;
-}
--- FE/klinikaStudyBase/src/app/login-page/login-page.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
-})
-export class LoginPageComponent {
-
-}
