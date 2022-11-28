dcbf45cb3098ead499f4ddb59ebd60d1d7ba06c7
davidbrijak <61884037+davidbrijak@users.noreply.github.com>
2022-11-28 09:18:45 +0100

    Delete BE directory

--- BE/src/main/java/com/projekt/klinikaStudyBase/dto/UserCredentialsDto.java
+++
@@ -1,9 +0,0 @@
-package com.projekt.klinikaStudyBase.dto;
-
-import lombok.Data;
-
-@Data
-public class UserCredentialsDto {
-    private String email;
-    private String password;
-}
--- BE/src/main/java/com/projekt/klinikaStudyBase/repository/FileDBRepository.java
+++
@@ -1,9 +0,0 @@
-package com.projekt.klinikaStudyBase.repository;
-
-import com.projekt.klinikaStudyBase.data.entity.FileDB;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FileDBRepository extends JpaRepository<FileDB, String> {
-}
